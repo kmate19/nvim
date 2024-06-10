5eabeb449df943c9becaaa849d55cd2c99f8286b
kmate19 <kissmate019@gmail.com>
2024-06-10 05:44:07 +0200

    idk[

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,5 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map("n", "KM", "<cmd>execute 'Man ' .. expand('<cword>')<CR>", { desc = "open man page for symbol" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
