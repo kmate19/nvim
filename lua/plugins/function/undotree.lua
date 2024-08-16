98b9e32ca3213aa3887407cd07b2ff8b4bf1d713
kmate19 <kissmate019@gmail.com>
2024-08-16 10:00:02 +0200

    added undotree bind

--- lua/plugins/function/undotree.lua
+++ lua/plugins/function/undotree.lua
@@ -1,3 +1,12 @@
 return {
-  'mbbill/undotree',
+  {
+    'mbbill/undotree',
+    opts = {},
+    config = function()
+      vim.keymap.set('n', '<leader>U', function()
+        vim.cmd.UndotreeToggle()
+        vim.cmd.UndotreeFocus()
+      end)
+    end,
+  },
 }
