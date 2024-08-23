1ac54f0e21d4bd8a144de204ca88ff51dbca7db6
kmate19 <kissmate019@gmail.com>
2024-08-23 11:44:14 +0200

    changed helpful prompt

--- lua/plugins/function/dingllm.lua
+++ lua/plugins/function/dingllm.lua
@@ -4,7 +4,8 @@ return {
   config = function()
     local system_prompt =
       'You should replace only the code that you are sent, only following the comments. Do not talk at all. Only output valid code. Do not provide any backticks that surround the code. Never ever output backticks like this ```. Any comment that is asking you for something should be removed after you satisfy them. Other comments should left alone. Do not output backticks'
-    local helpful_prompt = 'You are a helpful assistant. What I have sent are my notes so far.'
+    local helpful_prompt =
+      'You are a helpful assistant. What I have sent are my notes so far. If you detect a programming language, you MUST make every line you output a comment in that language!'
     local dingllm = require 'dingllm'
 
     -- local function handle_open_router_spec_data(data_stream)
