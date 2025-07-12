d00738d2dfac6d5d5f90afa19d65369a1381e455
kmate19 <kissmate019@gmail.com>
2025-07-12 11:15:03 +0200

    changed gitsign characters

--- lua/plugins/function/gitsigns.lua
+++ lua/plugins/function/gitsigns.lua
@@ -4,7 +4,24 @@ return {
     current_line_blame = true,
     numhl = true,
     current_line_blame_opts = {
-      delay = 400,
+      -- maybe dont auto show in the future make this a keybind
+      delay = 0,
+    },
+    signs = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '-' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~-' },
+      untracked = { text = '/' },
+    },
+    signs_staged = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '-' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~-' },
+      untracked = { text = '/' },
     },
   },
 }
