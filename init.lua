b31dcacd8243cb3e8d5eeb3d14769330117f09ed
kmate19 <99414737+kmate19@users.noreply.github.com>
2026-01-31 15:29:26 +0100

    colorscheme

--- init.lua
+++ init.lua
@@ -40,12 +40,11 @@ require("lazy").setup({
 	spec = {
 		{ import = "plugins" },
 		{
-			"folke/tokyonight.nvim",
-			lazy = false, -- make sure we load this during startup if it is your main colorscheme
-			priority = 1000, -- make sure to load this before all the other start plugins
-			config = function()
-				-- load the colorscheme here
-				vim.cmd.colorscheme("tokyonight")
+			"ellisonleao/gruvbox.nvim",
+			priority = 1000,
+			config = true,
+			init = function()
+				vim.cmd.colorscheme("gruvbox")
 			end,
 		},
 	},
